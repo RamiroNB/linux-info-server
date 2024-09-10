daa33bf6b06fb35dd8f984f20838c9c8280a3e2f
RamiroNB <ramiro.nilson.barros@gmail.com>
2024-09-10 13:04:25 -0300

    UPdate some stuff

--- simple_http_server_multiclient.c
+++ simple_http_server_multiclient.c
@@ -9,7 +9,6 @@
 #include <dirent.h>
 #include <ctype.h>
 #include <time.h>
-#include <sys/sysinfo.h>
 #include <fcntl.h>
 
 #define BUFLEN 8192
@@ -26,59 +25,136 @@ void die(char *s)
 
 void get_current_datetime(char *buffer, size_t buf_size)
 {
-	time_t now = time(NULL);
-	struct tm *tm_info = localtime(&now);
-	strftime(buffer + strlen(buffer), buf_size - strlen(buffer), "<h3>Date/Time: %c</h3>", tm_info);
+	// read from /proc/datetime
+	FILE *fp = fopen("/proc/datetime", "r");
+	if (fp)
+	{
+		char line[256];
+		if (fgets(line, sizeof(line), fp))
+		{
+			strncat(buffer, "<h3>Current Date/Time: ", buf_size - strlen(buffer) - 1);
+			strncat(buffer, line, buf_size - strlen(buffer) - 1);
+			strncat(buffer, "</h3>", buf_size - strlen(buffer) - 1);
+		}
+		fclose(fp);
+	}
+	else
+	{
+		strncat(buffer, "<h3>Current Date/Time: N/A</h3>", buf_size - strlen(buffer) - 1);
+	}
 }
 
-void get_uptime(char *buffer, size_t buf_size)
+void get_uptime(char *buffer, size_t len)
 {
-	struct sysinfo info;
-	sysinfo(&info);
-	snprintf(buffer + strlen(buffer), buf_size - strlen(buffer), "<h3>Uptime: %.2f seconds</h3>", info.uptime);
+	FILE *fp = fopen("/proc/uptime", "r");
+	if (fp == NULL)
+	{
+		snprintf(buffer, len, "N/A");
+	}
+	else
+	{
+		double uptime;
+		fscanf(fp, "%lf", &uptime);
+		snprintf(buffer, len, "<h3>Uptime: %.2f seconds</h3>", uptime);
+		fclose(fp);
+	}
 }
 
 void get_processor_info(char *buffer, size_t buf_size)
 {
+	// read from /proc/cpuinfo
+
 	FILE *fp = fopen("/proc/cpuinfo", "r");
 	if (fp)
 	{
 		char line[256];
 		while (fgets(line, sizeof(line), fp))
 		{
-			if (strncmp(line, "model name", 11) == 0)
+			if (strstr(line, "model name"))
 			{
-				strncat(buffer + strlen(buffer), "<h3>Processor: ", buf_size - strlen(buffer) - 1);
-				strncat(buffer + strlen(buffer), strchr(line, ':') + 2, buf_size - strlen(buffer) - 1);
-				strncat(buffer + strlen(buffer), "</h3>", buf_size - strlen(buffer) - 1);
+				strncat(buffer, "<h3>Processor: ", buf_size - strlen(buffer) - 1);
+				strncat(buffer, strchr(line, ':') + 2, buf_size - strlen(buffer) - 1);
+				strncat(buffer, "</h3>", buf_size - strlen(buffer) - 1);
 				break;
 			}
 		}
 		fclose(fp);
 	}
+	else
+	{
+		strncat(buffer, "<h3>Processor: N/A</h3>", buf_size - strlen(buffer) - 1);
+	}
+
+	// cpu usage
+	FILE *fp1 = fopen("/proc/stat", "r");
+
+	if (fp1)
+	{
+		char line[256];
+		long user = 0, nice = 0, system = 0, idle = 0;
+		while (fgets(line, sizeof(line), fp1))
+		{
+			if (sscanf(line, "cpu %ld %ld %ld %ld", &user, &nice, &system, &idle) == 4)
+			{
+				break;
+			}
+		}
+		long total = user + nice + system + idle;
+		long cpu_usage = 100 * (total - idle) / total;
+		snprintf(buffer + strlen(buffer), buf_size - strlen(buffer), "<h3>CPU Usage: %ld%%</h3>", cpu_usage);
+		fclose(fp1);
+	}
+	else
+	{
+		snprintf(buffer + strlen(buffer), buf_size - strlen(buffer), "<h3>CPU Usage: N/A</h3>");
+	}
 }
 
 void get_memory_usage(char *buffer, size_t buf_size)
 {
-	struct sysinfo info;
-	sysinfo(&info);
-	snprintf(buffer + strlen(buffer), buf_size - strlen(buffer), "<h3>Memory Usage: Total: %ld MB, Used: %ld MB</h3>",
-			 info.totalram / (1024 * 1024), (info.totalram - info.freeram) / (1024 * 1024));
+	// read from /proc/meminfo
+	FILE *fp = fopen("/proc/meminfo", "r");
+	if (fp)
+	{
+		char line[256];
+		long total_mem = 0, free_mem = 0;
+		while (fgets(line, sizeof(line), fp))
+		{
+			if (sscanf(line, "MemTotal: %ld", &total_mem) == 1 ||
+				sscanf(line, "MemFree: %ld", &free_mem) == 1)
+			{
+				if (total_mem && free_mem)
+					break;
+			}
+		}
+		snprintf(buffer + strlen(buffer), buf_size - strlen(buffer), "<h3>Memory Usage: Total: %ld MB, Used: %ld MB</h3>",
+				 total_mem / 1024, (total_mem - free_mem) / 1024);
+		fclose(fp);
+	}
+	else
+	{
+		snprintf(buffer + strlen(buffer), buf_size - strlen(buffer), "<h3>Memory Usage: N/A</h3>");
+	}
 }
 
 void get_kernel_version(char *buffer, size_t buf_size)
 {
+	// read from /proc/version
 	FILE *fp = fopen("/proc/version", "r");
 	if (fp)
 	{
 		char line[256];
 		if (fgets(line, sizeof(line), fp))
 		{
-			strncat(buffer + strlen(buffer), "<h3>Kernel Version: ", buf_size - strlen(buffer) - 1);
-			strncat(buffer + strlen(buffer), strchr(line, ':') + 2, buf_size - strlen(buffer) - 1);
-			strncat(buffer + strlen(buffer), "</h3>", buf_size - strlen(buffer) - 1);
+			strncat(buffer, "<h3>Kernel Version: ", buf_size - strlen(buffer) - 1);
+			strncat(buffer, line, buf_size - strlen(buffer) - 1);
+			strncat(buffer, "</h3>", buf_size - strlen(buffer) - 1);
 		}
 		fclose(fp);
+	}
+	else
+	{
+		strncat(buffer, "<h3>Kernel Version: N/A</h3>", buf_size - strlen(buffer) - 1);
 	}
 }
 
@@ -141,86 +217,77 @@ void get_processes(char *buffer, size_t buf_size, int page)
 
 void get_disks(char *buffer, size_t buf_size)
 {
-	strncat(buffer, "<h3>Disk Units</h3><ul>", buf_size - strlen(buffer) - 1);
-	FILE *fp = popen("df -h", "r");
-	char line[256];
-	while (fgets(line, sizeof(line), fp))
+	// read from /proc/partitions
+	FILE *fp = fopen("/proc/partitions", "r");
+	if (fp)
 	{
-		strncat(buffer, "<li>", buf_size - strlen(buffer) - 1);
-		strncat(buffer, line, buf_size - strlen(buffer) - 1);
-		strncat(buffer, "</li>", buf_size - strlen(buffer) - 1);
+		char line[256];
+		while (fgets(line, sizeof(line), fp))
+		{
+			if (strstr(line, "sd"))
+			{
+				strncat(buffer, "<h3>Disks</h3><ul>", buf_size - strlen(buffer) - 1);
+				strncat(buffer, "<li>", buf_size - strlen(buffer) - 1);
+				strncat(buffer, line, buf_size - strlen(buffer) - 1);
+				strncat(buffer, "</li>", buf_size - strlen(buffer) - 1);
+			}
+		}
+		fclose(fp);
 	}
-	pclose(fp);
-	strncat(buffer, "</ul><br>", buf_size - strlen(buffer) - 1);
+	else
+	{
+		strncat(buffer, "<h3>Disks: N/A</h3>", buf_size - strlen(buffer) - 1);
+	}
 }
 
 void get_usb_info(char *buffer, size_t buf_size)
 {
-	strncat(buffer, "<h3>USB Devices</h3><ul>", buf_size - strlen(buffer) - 1);
-
-	DIR *dir = opendir("/sys/bus/usb/devices");
-	struct dirent *entry;
-
-	if (!dir)
+	// read from /proc/bus/usb/devices
+	FILE *fp = fopen("/proc/bus/usb/devices", "r");
+	if (fp)
 	{
-		strncat(buffer, "Unable to open /sys/bus/usb/devices.<br>", buf_size - strlen(buffer) - 1);
-		return;
-	}
-
-	struct stat st;
-	char path[256];
-
-	while ((entry = readdir(dir)) != NULL)
-	{
-		snprintf(path, sizeof(path), "/sys/bus/usb/devices/%s", entry->d_name);
-
-		if (stat(path, &st) == 0 && S_ISDIR(st.st_mode))
+		char line[256];
+		while (fgets(line, sizeof(line), fp))
 		{
-			strncat(buffer, "<li>", buf_size - strlen(buffer) - 1);
-			strncat(buffer, entry->d_name, buf_size - strlen(buffer) - 1);
-			strncat(buffer, "</li>", buf_size - strlen(buffer) - 1);
+			if (strstr(line, "Product="))
+			{
+				strncat(buffer, "<h3>USB Devices</h3><ul>", buf_size - strlen(buffer) - 1);
+				strncat(buffer, "<li>", buf_size - strlen(buffer) - 1);
+				strncat(buffer, line, buf_size - strlen(buffer) - 1);
+				strncat(buffer, "</li>", buf_size - strlen(buffer) - 1);
+			}
 		}
+		fclose(fp);
 	}
-
-	closedir(dir);
-	strncat(buffer, "</ul><br>", buf_size - strlen(buffer) - 1);
+	else
+	{
+		strncat(buffer, "<h3>USB Devices: N/A</h3>", buf_size - strlen(buffer) - 1);
+	}
 }
 
 void get_network_adapters(char *buffer, size_t buf_size)
 {
-	strncat(buffer, "<h3>Network Adapters</h3><ul>", buf_size - strlen(buffer) - 1);
-
+	// read from /proc/net/dev
 	FILE *fp = fopen("/proc/net/dev", "r");
 	if (fp)
 	{
 		char line[256];
 		while (fgets(line, sizeof(line), fp))
 		{
-			if (strchr(line, ':'))
+			if (strstr(line, ":"))
 			{
-				char *iface = strtok(line, ":");
-				if (iface)
-				{
-					strncat(buffer, "<li>", buf_size - strlen(buffer) - 1);
-					strncat(buffer, iface, buf_size - strlen(buffer) - 1);
-					strncat(buffer, " - IP: ", buf_size - strlen(buffer) - 1);
-					char ip[64];
-					snprintf(ip, sizeof(ip), "ip -o -4 addr show %s | awk '{print $4}'", iface);
-					FILE *ip_fp = popen(ip, "r");
-					char ip_addr[32];
-					if (fgets(ip_addr, sizeof(ip_addr), ip_fp))
-					{
-						strncat(buffer, ip_addr, buf_size - strlen(buffer) - 1);
-					}
-					pclose(ip_fp);
-					strncat(buffer, "</li>", buf_size - strlen(buffer) - 1);
-				}
+				strncat(buffer, "<h3>Network Adapters</h3><ul>", buf_size - strlen(buffer) - 1);
+				strncat(buffer, "<li>", buf_size - strlen(buffer) - 1);
+				strncat(buffer, line, buf_size - strlen(buffer) - 1);
+				strncat(buffer, "</li>", buf_size - strlen(buffer) - 1);
 			}
 		}
 		fclose(fp);
 	}
-
-	strncat(buffer, "</ul><br>", buf_size - strlen(buffer) - 1);
+	else
+	{
+		strncat(buffer, "<h3>Network Adapters: N/A</h3>", buf_size - strlen(buffer) - 1);
+	}
 }
 
 int main(void)
